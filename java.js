de9dab07faf5250b37132c14bd1998a39ef86229
BenjaminSk09 <benjaminsincalaju@gmail.com>
2025-01-30 15:41:35 -0600

    Cambios en la pagina de habilitacion de facturas

--- java.js
+++ java.js
@@ -13,3 +13,22 @@ document.getElementById("scrollbutton").addEventListener("click", function(){
     document.getElementById("services").scrollIntoView({behaviorL: "smooth"});
 });
 
+//pagina facturas//
+document.addEventListener("DOMContentLoaded", function () {
+  // Agrega la clase "visible" a los elementos cuando aparecen en la pantalla
+  function revealOnScroll() {
+      let benefits = document.querySelectorAll(".animate-benefit");
+      let windowHeight = window.innerHeight;
+
+      benefits.forEach(function (benefit) {
+          let position = benefit.getBoundingClientRect().top;
+          if (position < windowHeight - 50) {
+              benefit.classList.add("visible");
+          }
+      });
+  }
+
+  // Llamar a la función cuando se haga scroll
+  window.addEventListener("scroll", revealOnScroll);
+  revealOnScroll(); // Para verificar al cargar la página
+});
